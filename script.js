0d93a2daa884b56d388dd640b26fc843446539b5
mizan341 <mizanur20140341@gmail.com>
2020-08-11 16:31:46 +0600

    aj notun kicu korlam

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("name-id").addEventListener("mouseover", function () {
+    document.getElementById("name-id-side").style.color = "tomato"
+});
+document.getElementById("name-id").addEventListener("mouseout", function () {
+    document.getElementById("name-id-side").style.color = "#03e9f4"
+});
